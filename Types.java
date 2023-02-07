758b76b7f04326aa6c465862c3d32f47133ae8de
Marvioo <guangjia@usc.edu>
2023-02-07 13:45:38 -0800

    added 4 files

---
+++ Types.java
@@ -0,0 +1,17 @@
+package com.cogent.basics;
+
+public class Types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isEqual = 5 == 8;
+		boolean isDifferent = 7 != 7;
+		int sum = 10 + 100;
+		double divide = 100.0 / 2;
+		int sub = 200 - 90;
+		boolean answer = isEqual && isDifferent;
+		sum += 1000;
+		boolean answer2 = !answer;
+	}
+
+}
